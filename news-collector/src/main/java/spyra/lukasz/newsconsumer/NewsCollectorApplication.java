85078e7b4f5f1d81933300e5c378fdc824e959b4
Lukasz Spyra <lukasz.spyrka@gmail.com>
2024-04-06 14:34:48 +0200

    Create news-collector microservice module

---
+++ news-collector/src/main/java/spyra/lukasz/newsconsumer/NewsCollectorApplication.java
@@ -0,0 +1,13 @@
+package spyra.lukasz.newsconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsCollectorApplication.class, args);
+	}
+
+}
---
+++ news-collector/src/test/java/spyra/lukasz/newsconsumer/NewsCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package spyra.lukasz.newsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
