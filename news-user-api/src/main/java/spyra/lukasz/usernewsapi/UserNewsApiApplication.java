78b06554c7d563036e9a8dd9c4bab97d5d5bb5d8
Lukasz Spyra <lukasz.spyrka@gmail.com>
2024-04-06 14:29:38 +0200

    Create news-user-api microservice module

---
+++ news-user-api/src/main/java/spyra/lukasz/usernewsapi/UserNewsApiApplication.java
@@ -0,0 +1,11 @@
+package spyra.lukasz.usernewsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNewsApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserNewsApiApplication.class, args);
+	}
+}
---
+++ news-user-api/src/test/java/spyra/lukasz/usernewsapi/UserNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spyra.lukasz.usernewsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
