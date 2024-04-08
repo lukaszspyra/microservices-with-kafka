d6f682514e630832b5805316d28237c9c0b7ef1c
Lukasz Spyra <lukasz.spyrka@gmail.com>
2024-04-09 01:30:34 +0200

    Create Kafka topic config

---
+++ news-collector/src/main/java/spyra/lukasz/newsconsumer/config/KafkaTopicNameProvider.java
@@ -0,0 +1,40 @@
+package spyra.lukasz.newsconsumer.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Provides kafka topic names
+ */
+@Component
+public class KafkaTopicNameProvider {
+
+  @Value("${spring.kafka.topic.news.request}")
+  private String newsRequest;
+
+  @Value("${spring.kafka.topic.news.response}")
+  private String newsResponse;
+
+  @Value("${spring.kafka.topic.json}")
+  private String jsonTopic;
+
+  @Value("${spring.kafka.topic.avro}")
+  private String avroTopic;
+
+  String newsRequest() {
+    return newsRequest;
+  }
+
+  public String newsResponse() {
+    return newsResponse;
+  }
+
+  String jsonTopic() {
+    return jsonTopic;
+  }
+
+  String avroTopic() {
+    return avroTopic;
+  }
+
+}
