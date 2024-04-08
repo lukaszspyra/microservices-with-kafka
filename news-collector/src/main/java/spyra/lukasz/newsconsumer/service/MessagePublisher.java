ca7f4cfc10b4ba7440fd61241b6016a148dae0a3
Lukasz Spyra <lukasz.spyrka@gmail.com>
2024-04-09 01:41:46 +0200

    Create MessageService with implementation publishing news response

---
+++ news-collector/src/main/java/spyra/lukasz/newsconsumer/service/MessagePublisher.java
@@ -0,0 +1,21 @@
+package spyra.lukasz.newsconsumer.service;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessagePublisher implements MessageService {
+  private final KafkaTemplate<String, String> kafkaTemplate;
+
+  public MessagePublisher(final KafkaTemplate<String, String> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  @Override
+  public void publishResponseMessage(String topic, String date, final String body) {
+    ProducerRecord<String, String> record = new ProducerRecord<>(topic, date, body);
+    kafkaTemplate.send(record);
+  }
+
+}
